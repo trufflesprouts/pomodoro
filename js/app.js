2ef4c69d075414a12deabbc18bba04763d32cbdc
trufflesprouts <milkyabdullah@gmail.com>
2016-11-27 13:52:06 +0300

    updated pomodoro clock

--- js/app.js
+++ js/app.js
@@ -1,89 +1,153 @@
-// Added a function to the string prototype for easy string replacement at any index
-String.prototype.replaceAt = function(index, character) {
-	return this.substr(0, index) + character + this.substr(index + character.length);
+var timer = {
+	runningDuration: 0, // Duration of the current timer, so the progress bar doesn't reset every time a user pauses
+	start: function(duration, display) {
+		var start = Date.now(),
+			self = this,
+			diff,
+			minutes,
+			seconds;
+
+		self.timerRunning = true;
+
+		function timerFunc() {
+			// get the number of seconds that have elapsed since startTimer() was called
+			diff = duration - (((Date.now() - start) / 1000) | 0);
+			// if we reached end of timer
+			if (diff <= 0) {
+				timer.stop();
+
+				// Play alarm tone
+				var audioEls = Array.from(document.getElementsByTagName('audio'));
+				if (audioEls[0]) {
+					document.body.removeChild(audioEls[0]); // If one already exists, remove it
+				}
+				var audioEl = document.createElement("audio");
+				audioEl.src = "./Shooting_star.ogg";
+				audioEl.autoplay = "true";
+				document.body.appendChild(audioEl);
+
+				// Style the timer box while the alarm is ringing
+				timerEl.style.backgroundColor = "#FF6A6A";
+				timerEl.style.color = "#fff"
+				timeEl.style.color = "#fff";
+				setTimeout(function () {
+					timerEl.style.backgroundColor = "#fff";
+					timerEl.style.color = "#000"
+					timeEl.style.color = "#FF6A6A";
+				}, 7000);
+
+				// Start the next session/break
+				if (timerType.textContent == "Session") {
+					timer.runningDuration = 60 * breakLength ;
+					timerType.textContent = "Break!";
+					timer.start(breakLength*60 + 1, timeEl);
+				} else {
+					timer.runningDuration = 60 * sessionLength;
+					timerType.textContent = "Session";
+					timer.start(sessionLength * 60 + 1, timeEl);
+				}
+
+			}
+
+			// Get the minutes and seconds and style them
+			minutes = parseInt(diff / 60, 10);
+			seconds = parseInt(diff % 60, 10);
+			minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+			// Progress bar
+			var progress = (self.runningDuration - diff) / self.runningDuration * 100;
+			progressBar.style.width = progress +"%";
+
+			currDuration = diff;
+
+			// Update webpage
+			titleEl.textContent = minutes + ":" + seconds + " " + timerType.textContent;
+			display.textContent = minutes + ":" + seconds;
+		};
+		// we don't want to wait a full second before the timer starts
+		timerFunc();
+		self.timerInterval = setInterval(timerFunc, 1000);
+
+	},
+	stop: function() {
+		this.timerRunning = false;
+		clearInterval(this.timerInterval);
+		this.timerInterval = null;
+	}
 }
 
-// Store UI element
-var calcBtnELs = Array.from(document.getElementsByClassName('calc-btn'));
-var screenEl = document.getElementById('calc-screen-text');
-var screenCalcsEl = document.getElementById('calc-screen-text-bottom');
-// Math operations array
-var mathOperations = ["+", "-", "/", "*"];
-// Initilize calculations
-var calculations = "";
+function styleTime(time) {
+	if (time < 10) {
+		return "0" + time + ":00";
+	} else {
+		return time + ":00";
+	}
+}
 
-// Add enent listener on every button
-for (var i = 0; i < calcBtnELs.length; i++) {
-	// Immiedietly invoked function to make use of closures
-	(function(index) {
-		calcBtnELs[index].addEventListener('click', function() {
-			var operation = calcBtnELs[index].getAttribute("operation");
-			// AC button
-			if (operation == "AC") {
-				screenEl.textContent = "";
-				calculations = "";
+var titleEl = document.getElementsByTagName('title')[0];
+var startBtnEl = document.getElementById('start-btn');
+var timerEl = document.getElementById('timer');
+var timeEl = document.getElementById('time');
+var timerType = document.getElementById('timer-type');
+var sessionLength = document.getElementById('sessionLength').textContent;
+var breakLength = document.getElementById('breakLength').textContent;
+var currDuration = 60 * sessionLength;
+timer.runningDuration = 60 * sessionLength;
+time.textContent = styleTime(sessionLength);
+var progressBar = document.getElementById('progress-bar');
+
+//////////////////// Timer Start/Pause ////////////////////
+startBtnEl.addEventListener("click", function () {
+	if (timeEl.textContent == "00:00") {
+		return;
+	}
+	if (timer.timerRunning) {
+		timer.stop();
+	} else {
+		timer.start(currDuration, timeEl);
+	}
+});
+
+//////////////////// Control Buttons ////////////////////
+var controlBtnEls = Array.from(document.querySelectorAll('.controls button'));
+for (var i = 0; i < controlBtnEls.length; i++) {
+	controlBtnEls[i].addEventListener("click", function () {
+		self = this;
+		var counterEl = this.parentNode.childNodes[3];
+		if (this.value == "add") {
+			if (counterEl.textContent < 60) {
+				counterEl.textContent = (counterEl.textContent | 0) + 5;
 			}
-			// CE button
-			if (operation == "CE") {
-				screenEl.textContent = screenEl.textContent.substr(0, screenEl.textContent.length - 1);
-				calculations = calculations.replace(/([\/\+\-\*])[0-9]*$|^[0-9]*$/, "$1" + screenEl.textContent);
+		} else {
+			if (counterEl.textContent > 5) {
+				counterEl.textContent = (counterEl.textContent | 0) - 5;
 			}
-			// Squareroot button
-			if (operation == "√") {
-				screenEl.textContent = Math.sqrt(screenEl.textContent);
-				calculations = calculations.replace(/([\/\+\-\*])[0-9]*$|^[0-9]*$/, "$1" + screenEl.textContent);
-        if (screenEl.textContent.length > 16) {
-					var currNum = Number(screenEl.textContent);
-					screenEl.textContent = currNum.toPrecision(10);
-					calculations = screenEl.textContent;
-				}
-			}
-			// Stop taking input if there isn't enough space on screen
-			if ((/[0-9]/.test(operation) || operation == ".") && (screenEl.textContent.length > 15 || calculations.length > 24)) {
-				return;
-			}
-			// Number buttons (and dot)
-			if (/[0-9]/.test(operation) || operation == ".") {
-				// Don't accept a DOT if one is already on screen
-				if (operation == "." && screenEl.textContent.indexOf(".") !== -1) {
-					return;
-				}
-				// Clean math operation off the screen
-				if (mathOperations.indexOf(screenEl.textContent) !== -1) {
-					screenEl.textContent = "";
-				}
-				// Update screen
-				screenEl.textContent += operation;
-				calculations += operation;
-			}
-			// Math operation buttons
-			if (mathOperations.indexOf(operation) !== -1) {
-				// If there's already an operation replace it the new one
-				if (mathOperations.indexOf(calculations[calculations.length - 1]) !== -1) {
-					calculations = calculations.replaceAt(calculations.length - 1, operation);
-					screenEl.textContent = screenEl.textContent.replaceAt(0, operation);
-					screenCalcsEl.textContent = calculations;
-					return;
-				}
-				// Add operation to the screen
-				screenEl.textContent = operation;
-				calculations += operation;
-			}
-			// Result button
-			if (operation == "=") {
-				// View result on screen
-				screenEl.textContent = eval(calculations);
-				calculations = eval(calculations);
-				// If result is too long, shorten result
-				if (screenEl.textContent.length > 16) {
-					var currNum = Number(screenEl.textContent);
-					screenEl.textContent = currNum.toPrecision(10);
-					calculations = screenEl.textContent;
-				}
-				screenCalcsEl.textContent = "";
-				return;
-			}
-			screenCalcsEl.textContent = calculations;
-		});
-	})(i);
-};
+		}
+	});
+}
+
+//////////////////// Reset Button ////////////////////
+var resetBtnEl = document.getElementById('reset-btn');
+resetBtnEl.addEventListener("click", function() {
+	timer.stop();
+
+	sessionLength = document.getElementById('sessionLength').textContent;
+	breakLength = document.getElementById('breakLength').textContent;
+	currDuration = 60 * sessionLength;
+
+	titleEl.textContent = "Pomodoro Clock";
+	timerType.textContent = "Session";
+	timer.runningDuration = 60 * sessionLength;
+	time.textContent = styleTime(sessionLength);
+
+	progressBar.style.width = "0%";
+	timerEl.style.backgroundColor = "#fff";
+	timerEl.style.color = "#000"
+	timeEl.style.color = "#FF6A6A";
+
+	var audioEl = Array.from(document.getElementsByTagName('audio'));
+	if (audioEl[0]) {
+		document.body.removeChild(audioEl[0]);
+	}
+});
